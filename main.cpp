49733e5e4d632b651d98efe8fba89b1478e6a415
neiljohnson <mr.neil.j@gmail.com>
2017-02-07 20:03:20 -0800

    Made a mistake in array definition.  Adjusted to show properly.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void showValues(int intArray[], int size);  // Function prototype
 int main()
 {
     const int ARRAY_SIZE = 8;
-    int collection [ARRAY_SIZE] = {5, 10, 15, 20, 25, 30, 40};
+    int collection [ARRAY_SIZE] = {5, 10, 15, 20, 25, 30, 35, 40};
 
     cout << "The array contains the values\n";
     showValues(collection, ARRAY_SIZE);
